a75a790eae30c46aa990c6aefb6e56f28250f899
Insu Byeon <yuki@yuki.kr>
2020-09-04 15:13:33 +0900

    Initial Commit

---
+++ BISLogger/BISLogger.h
@@ -0,0 +1,19 @@
+//
+//  BISLogger.h
+//  BISLogger
+//
+//  Created by Insu Byeon on 2020/09/04.
+//  Copyright © 2020 Insu Byeon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BISLogger.
+FOUNDATION_EXPORT double BISLoggerVersionNumber;
+
+//! Project version string for BISLogger.
+FOUNDATION_EXPORT const unsigned char BISLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BISLogger/PublicHeader.h>
+
+
